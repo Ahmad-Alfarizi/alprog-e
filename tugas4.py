3e8abcd4ead47a662a9e87d4254c660a7c8d990c
Ahmad Alfarizi <freyanahmad@gmail.com>
2024-10-25 15:43:05 +0700

    [ADD] menambahkan file tugas4

---
+++ tugas4.py
@@ -0,0 +1,10 @@
+name = "Ahmad Alfarizi"
+address = "Sikur, Lombok Timur"
+nim = "24241185"
+firstNim = 24
+lastNim = 85
+result = firstNim + lastNim
+
+print(f" Nama saya adalah {name}, \n Saya berasal dari {address}, \n nim saya {nim}")
+print(f" hasil penjumlahan dari {firstNim} + {lastNim} = {result} ")
+
