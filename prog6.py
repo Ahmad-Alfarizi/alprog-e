6fd8669ca59a405871c21e7151ff7eca36b7fcb9
Ahmad Alfarizi <freyanahmad@gmail.com>
2024-11-08 08:43:41 +0700

    [ADD] menambahkakn tugas baru prog6.py

---
+++ prog6.py
@@ -0,0 +1,30 @@
+x = 9
+y = 6
+print("nilai dari x : ", x)
+
+
+# aturan penulisan variable
+luas = 30 
+ahmad_alfarizi = 100 # jika lebih dari satu kata, gabung dengan tanda " _ "
+alfarizi = 20
+Ahmad = 3 # boleh gabungan capital non-capital
+ahmad = 14
+sanji88 = 10 # jika menggabungkan huruf dan angka, angka tidak boleh berada di posisi awal
+print("Nilai dari sanji", sanji88)
+
+
+#tipe data
+a = 4 # integer bilangan bulat
+print("tipe data dari  a : ", type(a)) 
+
+b = 6.9 # float bilangan pecahan
+print("tipe data dari b :", type(b))
+
+c = "Ahmad Alfarizi"
+print("tipe data dari c :", type(c))
+
+d = False
+print("tipe data dari d : ", type(d))
+
+
+
