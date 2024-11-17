3a77c33eb43e71ab19a661c0b554de2157b285a6
Ahmad Alfarizi <freyanahmad@gmail.com>
2024-11-17 11:14:19 +0700

    [ADD] menambahkan tugas baru

---
+++ tugas7.py
@@ -0,0 +1,10 @@
+# Menghitung Segitiga 
+# nama : Ahmad Alfarizi
+
+print("===== Menghitung Segitiga =====")
+alas = int(input("Masukkan Alas : "))
+tinggi = int(input("Masukkan Tinggi : "))
+
+hasil = 0.5 * alas * tinggi
+print(f"Hasil dari 1/2 x {alas} x {tinggi} adalah : {hasil}")
+
