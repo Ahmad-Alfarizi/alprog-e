f686ca6f2856f2bae27a6b06496052e374003c6e
Ahmad Alfarizi <freyanahmad@gmail.com>
2024-11-29 08:34:28 +0700

    [ADD] menambahkan tugas baru

---
+++ prog8.py
@@ -0,0 +1,31 @@
+x = 24
+y = 85
+
+# Penjumlahan
+hasil = x + y
+print("nilai dari x + y = ", hasil)
+
+# Pengurangan
+hasil = x - y
+print("hasil dari x - y = ", hasil)
+
+# Pembagian 
+hasil = x / y
+print("hasil dari x / y = ", hasil)
+
+# Perkalian
+hasil = x * y
+print("hasil dari x * y =", hasil)
+
+# Eksponen
+hasil = x ** y
+print("hasil dari x ** y = ", hasil)
+
+# Modulo
+hasil = x % y
+print("hasil dari x % y = ", hasil)
+
+# Floor division
+hasil = x // y
+print("hasil dari x // y = ", hasil)
+
